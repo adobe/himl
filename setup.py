aacc85224019484250c2a7c12fc5fe32486e6548
Razvan Dobre <dobre@adobe.com>
2025-07-28 12:18:22 +0300

    Add pathlib2 dependency

--- setup.py
+++ setup.py
@@ -11,6 +11,7 @@ _install_requires = [
     'lru_cache==0.2.3',
     'backports.functools_lru_cache==1.6.6',
     'pyyaml~=6.0.2',
+    'pathlib2~=2.3.7',
     'boto3~=1.35.94',
     'hvac~=2.3.0'
 ]
